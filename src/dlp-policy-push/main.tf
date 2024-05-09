4177fb0ca742fc8be0127273ecdf5a464c53ed06
Raphael Pothin <raphael.pothin@gmail.com>
2024-05-09 18:33:12 +0000

    Improvement of the dlp-policy-push infra as code

---
+++ src/dlp-policy-push/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    powerplatform = {
+      source  = "microsoft/power-platform"
+      version = "2.0.2-preview"
+    }
+  }
+
+  backend "azurerm" {
+    use_oidc = true
+  }
+}
+
+provider "powerplatform" {
+  use_oidc = true
+}
+
+data "powerplatform_connectors" "all_connectors" {}
+
+resource "powerplatform_data_loss_prevention_policy" "policy" {
+  display_name                      = var.display_name
+  default_connectors_classification = "Blocked"
+  environment_type                  = var.environment_type
+  environments                      = var.environments
+
+  business_connectors     = var.business_connectors
+  
+  # Dynamically generate non-business connectors based on the business connectors specified
+  non_business_connectors = [for conn in data.powerplatform_connectors.all_connectors.connectors : {
+    id                           = conn.id
+    name                         = conn.name
+    default_action_rule_behavior = ""
+    action_rules                 = [],
+    endpoint_rules               = []
+  } if conn.unblockable == true && !contains([for bus_conn in var.business_connectors : bus_conn.id], conn.id)]
+
+  # Dynamically generate blocked connectors based on the business connectors specified
+  blocked_connectors      = [for conn in data.powerplatform_connectors.all_connectors.connectors : {
+    id                           = conn.id
+    default_action_rule_behavior = ""
+    action_rules                 = [],
+    endpoint_rules               = []
+  } if conn.unblockable == false && !contains([for bus_conn in var.business_connectors : bus_conn.id], conn.id)]
+
+  custom_connectors_patterns = var.custom_connectors
+}
---
+++ src/dlp-policy-push/variables.tf
@@ -0,0 +1,42 @@
+# Define variables for the DLP policy configuration, including the business connectors group.
+variable "display_name" {
+  description = "The display name of the DLP policy."
+  type        = string
+}
+
+variable "environment_type" {
+  description = "Default environment handling for the policy (AllEnvironments, ExceptEnvironments, OnlyEnvironments)."
+  type        = string
+  default     = "OnlyEnvironments"
+}
+
+variable "environments" {
+  description = "A list of environment IDs to apply the DLP policy to."
+  type        = list(string)
+}
+
+variable "business_connectors" {
+  description = "A set of business connectors configurations."
+  type        = set(object({
+    id                           = string
+    default_action_rule_behavior = string
+    action_rules                 = list(object({
+      action_id = string
+      behavior  = string
+    }))
+    endpoint_rules               = list(object({
+      behavior = string
+      endpoint = string
+      order    = number
+    }))
+  }))
+}
+
+variable "custom_connectors" {
+  description = "A set of custom connectors configurations."
+  type        = set(object({
+    order            = number
+    host_url_pattern = string
+    data_group       = string
+  }))
+}

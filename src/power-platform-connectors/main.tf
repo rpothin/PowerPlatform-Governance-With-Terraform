e98233cbc77f21d46750de2c91316ad65f62e0be
Raphael Pothin <raphael.pothin@gmail.com>
2024-05-23 03:21:41 +0000

    Initialize power-platform-connectors Terraform configuration + a GitHub workflow to extract the output as a JSON file

---
+++ src/power-platform-connectors/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    powerplatform = {
+      source  = "microsoft/power-platform"
+      version = "2.0.2-preview"
+    }
+  }
+
+  backend "azurerm" {
+    use_oidc = true
+  }
+}
+
+provider "powerplatform" {
+  use_oidc = true
+}
+
+data "powerplatform_connectors" "all_connectors" {}
+
+output "all_connectors" {
+  value = data.powerplatform_connectors.all_connectors
+  description = "All power platform connectors"
+}
